8a73e52b42604d491d21973ed9bd78274a91cd72
mapevoque <makc261196@gmail.com>
2018-10-03 09:38:49 +0300

    HELP

--- t3.js
+++ t3.js
@@ -1,23 +1,19 @@
 describe('Describe', function() {
 browser.waitForAngularEnabled(false);
 browser.ignoreSynchronization = true;
-search = element(by.xpath('//*[@id="reg_submit11"]'));
+search = element(by.id('reg_submit4'));
 
 beforeEach(function() {
 browser.get('https://profile.tut.by/register.html');
 });
 
     it("test1", function () {
-	
-    search.isDisplayed().then(function(search){
-		if(search === true){		
-		console.log('EEEEEEE' + search);
-		}else{
-		browser.wait(function(search){
-		browser.element(search);}, 15000);
+		try{
+       search.isDisplayed();
+
 		}
-	}) 
-	
-    
-    });
+		catch(e) {
+		console.log('DDDDDD');
+		}
+})
 });
