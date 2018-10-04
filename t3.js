f0c2eb57cee66b54d7a49f7978962af0722cb22b
mapevoque <makc261196@gmail.com>
2018-10-05 00:09:40 +0300

    URAAAAAAA

--- t3.js
+++ t3.js
@@ -2,13 +2,17 @@ describe('Describe', function() {
 browser.waitForAngularEnabled(false);
 //browser.ignoreSynchronization = true;
 //By = browser.By;
-var a;
 until = browser.until;
 search = element(by.id('reg_submit'));
 
 var i = 1;
 function inc(){ 
   return i++ ;
+}
+
+var b = 1;
+function inc2(){ 
+  return b++ ;
 }
 
 function sleep(delay) {
@@ -21,19 +25,26 @@ browser.get('https://profile.tut.by/register.html');
 });
 
     it("test1", function () {
+		check1 = search.isPresent().then(function firstcheck(resolve, reject){
+			if(resolve == true){
 value = search.isEnabled().then(function wiii(resolve, reject){
 	if(resolve == true){
-		return true;
+		console.log('Объект дотупен');
 	}else{
 		if(resolve == false){
 			sleep(5000);
-			console.log('Дошли до ИФА');
-			if(inc() == 3){console.log('ПРЕВЫШЕН ПОХОД'); return true;}else{console.log('Вызов функции'); wiii(resolve);}
+			if(inc() == 3){console.log('Объект недоступен, проверьте объект'); return true;}else{console.log('Вызов функции'); wiii(resolve);}
 			
 		}
 	}
 	
 });
+			}else{
+				sleep(7000);
+				if(inc2() == 3){console.log('Объекта нет в DOM, проверьте локатор'); return true;}else{console.log('Вызов функции'); firstcheck(resolve);}
+				
+			}
+}); 
 
 
 });
