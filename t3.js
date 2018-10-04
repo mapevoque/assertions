dce87eb6466dd66e66791cd2c01b863ada36e03e
mapevoque <makc261196@gmail.com>
2018-10-04 10:02:11 +0300

    HELP3

--- t3.js
+++ t3.js
@@ -2,17 +2,30 @@ describe('Describe', function() {
 browser.waitForAngularEnabled(false);
 //browser.ignoreSynchronization = true;
 //By = browser.By;
-//until = browser.until;
-search = element(by.id('reg_submit'));
+until = browser.until;
+search = element(by.id('reg_submit5'));
 
 beforeEach(function() {
 browser.get('https://profile.tut.by/register.html');
 });
 
     it("test1", function () {
- rir = browser.wait(search,15000).isDisplayed().then(function(rir){
- console.log("POWER " + rir);
- });
+//expect(search.isDisplayed()).toBeFalsy("Ошибка");
+//expect(browser.wait(search,15000).isDisplayed()).toBeTruthy("Ошибка");
+browser.wait( async () => {
+	await search.isDisplayed();
+	//var EC = ExpectedConditions;
+    //browser.wait(EC.visibilityOf(search), 10000);
+    //return search;
+},15000);
+
+	
+//var until = protractor.ExpectedConditions;
+//browser.wait(until.search, 15000, 'WAITING');		
+		
+//rir = browser.wait(search,15000).then(function(rir){
+//console.log("POWER " + rir);
+//});
 });
 
 });
