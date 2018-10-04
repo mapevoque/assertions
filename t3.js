db21a6893209c30e4f8720b4e4a15c89f1cf67cf
mapevoque <makc261196@gmail.com>
2018-10-04 22:37:01 +0300

    HELP4

--- t3.js
+++ t3.js
@@ -2,30 +2,39 @@ describe('Describe', function() {
 browser.waitForAngularEnabled(false);
 //browser.ignoreSynchronization = true;
 //By = browser.By;
+var a;
 until = browser.until;
-search = element(by.id('reg_submit5'));
+search = element(by.id('reg_submit'));
 
-beforeEach(function() {
+var i = 1;
+function inc(){ 
+  return i++ ;
+}
+
+function sleep(delay) {
+    var start = new Date().getTime();
+    while (new Date().getTime() < start + delay);
+}
+
+beforeEach(function () {
 browser.get('https://profile.tut.by/register.html');
 });
 
     it("test1", function () {
-//expect(search.isDisplayed()).toBeFalsy("Ошибка");
-//expect(browser.wait(search,15000).isDisplayed()).toBeTruthy("Ошибка");
-browser.wait( async () => {
-	await search.isDisplayed();
-	//var EC = ExpectedConditions;
-    //browser.wait(EC.visibilityOf(search), 10000);
-    //return search;
-},15000);
-
+value = search.isEnabled().then(function wiii(resolve, reject){
+	if(resolve == true){
+		return true;
+	}else{
+		if(resolve == false){
+			sleep(5000);
+			console.log('Дошли до ИФА');
+			if(inc() == 3){console.log('ПРЕВЫШЕН ПОХОД'); return true;}else{console.log('Вызов функции'); wiii(resolve);}
+			
+		}
+	}
 	
-//var until = protractor.ExpectedConditions;
-//browser.wait(until.search, 15000, 'WAITING');		
-		
-//rir = browser.wait(search,15000).then(function(rir){
-//console.log("POWER " + rir);
-//});
 });
 
+
+});
 });
